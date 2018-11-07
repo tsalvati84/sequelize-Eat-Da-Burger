b7e6f2d7d5ebd202465a94b68c0faabcb19f1817
Tony Salvati <tsal2184@gmail.com>
2018-11-07 14:35:21 -0700

    Updated Eat-Da-Burger

---
+++ DB/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smokehouse BBQ Angus Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Brisket Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grass-Fed Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', FALSE);
---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+
+app.use(express.static("public"));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_Controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function () {
+    console.log("Listening on port:" + PORT);
+});
